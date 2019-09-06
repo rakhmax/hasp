f0d10c7a1493f1b23c8bd0ca8f4684f11c3eb6da
Максим Рахманин <rakhmaxx@gmail.com>
2019-09-06 22:37:28 +0300

    Add project files.

---
+++ FormMain.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace HSIS_lab_1
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBoxMain = new System.Windows.Forms.RichTextBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.LabelTrialInfo = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // richTextBoxMain
+            // 
+            this.richTextBoxMain.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBoxMain.Location = new System.Drawing.Point(12, 12);
+            this.richTextBoxMain.Name = "richTextBoxMain";
+            this.richTextBoxMain.Size = new System.Drawing.Size(776, 397);
+            this.richTextBoxMain.TabIndex = 0;
+            this.richTextBoxMain.Text = "";
+            this.richTextBoxMain.TextChanged += new System.EventHandler(this.RichTextBoxMain_TextChanged);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(713, 467);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 1;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.ButtonSave_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(12, 467);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 2;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.ButtonClear_Click);
+            // 
+            // LabelTrialInfo
+            // 
+            this.LabelTrialInfo.AutoSize = true;
+            this.LabelTrialInfo.Location = new System.Drawing.Point(660, 441);
+            this.LabelTrialInfo.Name = "LabelTrialInfo";
+            this.LabelTrialInfo.Size = new System.Drawing.Size(0, 13);
+            this.LabelTrialInfo.TabIndex = 3;
+            this.LabelTrialInfo.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 441);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(110, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "License activated";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 502);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.LabelTrialInfo);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.richTextBoxMain);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormMain_FormClosed);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxMain;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Label LabelTrialInfo;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ FormMain.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace HSIS_lab_1
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+
+            Properties.Settings.Default.number_of_starts++;
+            Properties.Settings.Default.Save();
+        }
+
+        private void RichTextBoxMain_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonClear_Click(object sender, EventArgs e)
+        {
+            richTextBoxMain.Clear();
+        }
+
+        private void ButtonSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                DefaultExt = "*.txt",
+                Filter = "Text files|*.txt"
+            };
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK && saveFileDialog.FileName.Length > 0)
+            {
+                using (StreamWriter sw = new StreamWriter(saveFileDialog.FileName, true))
+                {
+                    sw.WriteLine(richTextBoxMain.Text);
+                    sw.Close();
+                }
+            }
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            LabelTrialInfo.Text = Properties.Settings.Default.number_of_starts.ToString();
+
+            if (Properties.Settings.Default.number_of_starts >= 5)
+            {
+                var res = MessageBox.Show(
+                    "Evaluation is expired! Insert your HASP HL key",
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information
+                );
+
+                if (res == DialogResult.OK)
+                {
+                    Close();
+                }
+            }
+        }
+
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void FormMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            
+        }
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                LabelTrialInfo.Text = "";
+            }
+            else
+            {
+                LabelTrialInfo.Text = "You are using trial version";
+                LabelTrialInfo.ForeColor = Color.Red;
+            }
+        }
+    }
+}
